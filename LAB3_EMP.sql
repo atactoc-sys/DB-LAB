9aabdd85332bbe41d4682c98ff688b6e20e0082a
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-08-26 10:38:41 +0530

    Update LAB3_EMP.sql

--- LAB3_EMP.sql
+++ LAB3_EMP.sql
@@ -56,6 +56,6 @@ select * from employee;
 #18 Display the details of all those employees who are either CLERK or PROGRAMMER or ASSISTANT.
 select * from employee where job_id in('SH_CLERK','AD_ASST','IT_PROG');
 #19  Display those employees from the EMPLOYEE table whose designation is CLERK and salary is less than 3000.
-select * from employee where Job_id like '%CLERK%' and salary<3000;
+select * from employee where Job_id like 'CLERK' and salary<3000;
 #20 Display those employees Last_Name, Mgr_id from the EMPLOYEE table whose salary is above 3000 and work under Manager 101.
 select last_name, mgr_id from employee where salary>3000 and mgr_id=101;
